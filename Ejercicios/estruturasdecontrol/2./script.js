51716f0842a85e9300825ff275feb57433eb117b
Marcos <a21marcosfv@iessanclemente.net>
2024-10-07 17:29:37 +0200

    a

---
+++ Ejercicios/estruturasdecontrol/2./script.js
@@ -0,0 +1,16 @@
+let a = 1
+let b = 4
+let c = 3
+
+
+if (a > b && a > c) {
+    console.log(a)
+    
+} else if(b > a && b > c) {
+    console.log(b)
+
+} else if (c > a && c > b) {
+    console.log(c)
+    
+}
+    
---
+++ Ejercicios/estruturasdecontrol/3./script.js
@@ -0,0 +1,6 @@
+for (let a= 0; a <31 ; a++) {
+    if (a%2==0) {
+    console.log(a);
+    }
+
+}
